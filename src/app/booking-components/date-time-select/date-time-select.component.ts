b574ea1e090a04534883afe87f1e909f92cb5fd7
Ceeessan <Cessan__94@hotmail.com>
2025-06-16 19:35:17 +0200

    Fixing calendar for booking-functionality- startup

--- src/app/booking-components/date-time-select/date-time-select.component.ts
+++ src/app/booking-components/date-time-select/date-time-select.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 
 @Component({
   selector: 'app-date-time-select',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule, FullCalendarModule],
   templateUrl: './date-time-select.component.html',
   styleUrl: './date-time-select.component.scss'
 })
 export class DateTimeSelectComponent {
-
+  calendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridWeek',
+    weekends: true
+  };
 }
