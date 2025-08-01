680427b00528034e2535b60e088f42ff8ac7807d
Ceeessan <Cessan__94@hotmail.com>
2025-08-01 20:01:13 +0200

    Adding interval API ping for backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,17 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bookmyhair';
+
+  constructor( private http: HttpClient){}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.http.get('https://bookmyhair-backend.onrender.com/api/ping').subscribe({
+        next: () => console.log('pinged backend'),
+        error: (err) => console.error('ping error', err)
+      });
+    }, 600000);
+  }
 }
